21043dd552d8f2fcc30b9578b7a2e79c4f337529
Кирилл <kiryuha_21@mail.ru>
2021-12-24 23:12:02 +0300

    for report

--- test/controllers/tests_helper.rb
+++ test/controllers/tests_helper.rb
@@ -1,23 +1,23 @@
 module TestsHelper
-def create_user
-  @user = User.new(real_name: 'Кирилл', nickname: 'kiryuha12', password_digest: '1234')
-  @nickname = Faker::Name.first_name
-  post users_url,
-       params: { user: { nickname: @nickname, password_conf: @user.password_digest,
-                         password: @user.password_digest, real_name: @user.real_name } }
-  post session_create_url, params: { user: @nickname, password: @user.password_digest }
-  @user = User.find_by_nickname @nickname
-end
+  def create_user
+    @user = User.new(real_name: 'Кирилл', nickname: 'kiryuha12', password_digest: '1234')
+    @nickname = Faker::Name.first_name
+    post users_url,
+         params: { user: { nickname: @nickname, password_conf: @user.password_digest,
+                           password: @user.password_digest, real_name: @user.real_name } }
+    post session_create_url, params: { user: @nickname, password: @user.password_digest }
+    @user = User.find_by_nickname @nickname
+  end
 
-def save_game
-  @game_title = Faker::Game.title
-  post games_url, params: { game: { name: @game_title, creator: @nickname } }
-  @game = Game.find_by_name @game_title
-end
+  def save_game
+    @game_title = Faker::Game.title
+    post games_url, params: { game: { name: @game_title, creator: @nickname } }
+    @game = Game.find_by_name @game_title
+  end
 
-def create_post
-  @post_title = Faker::Game.title
-  post posts_url, params: { post: { title: @post_title } }
-  @post = Post.find_by_title @post_title
-end
+  def create_post
+    @post_title = Faker::Game.title
+    post posts_url, params: { post: { title: @post_title } }
+    @post = Post.find_by_title @post_title
+  end
 end
